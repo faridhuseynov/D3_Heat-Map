55e0047348a8b96f209a709a5a28f5ff214858c0
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-21 16:29:23 +0400

    Description texts added

--- index.js
+++ index.js
@@ -4,38 +4,52 @@ var data = [];
 const height = 600;
 const width = 1200;
 const padding = 80;
-var start_year="";
-var end_year="";
-
+var start_year = "";
+var end_year = "";
+var baseTemp = "";
 $(document).ready(() => {
     fetch(url)
         .then(response => response.json())
         .then(result => {
             data = result;
+            start_year = (data.monthlyVariance)[0].year;
+            var arrayLength = data.monthlyVariance.length;
+            end_year = (data.monthlyVariance)[arrayLength - 1].year;
+            baseTemp = data.baseTemperature;
             console.log(data);
-        })
-})
+            console.log(start_year, end_year, baseTemp);
 
-d3
-    .select("body")
-    .append("title").attr("id", "title")
-    .text("Heat Map");
 
-const svg = d3
-    .select("#main")
-    .append("svg")
-    .attr("id","canvas")
-    .attr("height",height)
-    .attr("width",width);
+            d3
+                .select("body")
+                .append("title").attr("id", "title")
+                .text("Heat Map");
 
-const descriptionBlock = svg
-    .append("g")
-    .attr("id","description");
+            const svg = d3
+                .select("#main")
+                .append("svg")
+                .attr("id", "canvas")
+                .attr("height", height)
+                .attr("width", width);
 
-descriptionBlock
-    .append("text")
-    .attr("x",(width-padding)/2)
-    .attr("y",padding)
-    .style("font-size","30px")
-    .attr("text-anchor","center")
-    .text("Monthly Global Land-Surface Temperature")
+            const descriptionBlock = svg
+                .append("g")
+                .attr("id", "description");
+
+            descriptionBlock
+                .append("text")
+                .attr("x", (width - padding) / 2)
+                .attr("y", padding)
+                .style("font-size", "30px")
+                .attr("text-anchor", "center")
+                .text("Monthly Global Land-Surface Temperature")
+
+            descriptionBlock
+                .append("text")
+                .attr("x", (width-padding) / 2+100)
+                .attr("y", padding + 50)
+                .style("font-size", "20px")
+                .attr("text-anchor", "center")
+                .text(start_year + " - " + end_year + ": base temperature " + baseTemp + "℃");
+        });
+});
