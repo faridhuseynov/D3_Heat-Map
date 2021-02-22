3c3dec6f9fbfe8b0e21cc23adceae18de80639fb
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-22 17:08:18 +0400

    Tooltip added and set up as per test requirements

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+var url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 var data = [];
 
 const height = 600;
@@ -8,130 +9,146 @@ var start_year = "";
 var end_year = "";
 var baseTemp = "";
 $(document).ready(() => {
-    fetch(url)
-        .then(response => response.json())
-        .then(result => {
-            data = result;
-            start_year = (data.monthlyVariance)[0].year;
-            var arrayLength = data.monthlyVariance.length;
-            end_year = (data.monthlyVariance)[arrayLength - 1].year;
-            baseTemp = data.baseTemperature;
-            console.log(data);
-            console.log(start_year, end_year, baseTemp);
-         
-            d3
-                .select("body")
-                .append("title").attr("id", "title")
-                .text("Heat Map");
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      data = result;
+      start_year = data.monthlyVariance[0].year;
+      var arrayLength = data.monthlyVariance.length;
+      end_year = data.monthlyVariance[arrayLength - 1].year;
+      baseTemp = data.baseTemperature;
+      console.log(data);
+      console.log(start_year, end_year, baseTemp);
 
-            const svg = d3
-                .select("#main")
-                .append("svg")
-                .attr("id", "canvas")
-                .attr("height", height)
-                .attr("width", width);
+      d3.select("body").append("title").attr("id", "title").text("Heat Map");
 
-            const descriptionBlock = svg
-                .append("g")
-                .attr("id", "description")
-                .attr("transform","translate("+(width/2)+",30)");
+      const svg = d3
+        .select("#main")
+        .append("svg")
+        .attr("id", "canvas")
+        .attr("height", height)
+        .attr("width", width);
 
-            descriptionBlock
-                .append("text")
-                .style("font-size", "30px")
-                .attr("text-anchor", "center")
-                .text("Monthly Global Land-Surface Temperature")
+      const descriptionBlock = svg
+        .append("g")
+        .attr("id", "description")
+        .attr("transform", "translate(" + width / 2 + ",30)");
 
-            descriptionBlock
-                .append("text")
-                .style("font-size", "20px")
-                .attr("x",padding)
-                .attr("y",padding/2)
-                .attr("text-anchor", "center")
-                .text(start_year + " - " + end_year + ": base temperature " + baseTemp + "℃");
+      descriptionBlock
+        .append("text")
+        .style("font-size", "30px")
+        .attr("text-anchor", "center")
+        .text("Monthly Global Land-Surface Temperature");
 
-            const xScale = d3
-                .scaleLinear()
-                .domain([start_year, end_year])
-                .range([padding, width - padding]);
+      descriptionBlock
+        .append("text")
+        .style("font-size", "20px")
+        .attr("x", padding)
+        .attr("y", padding / 2)
+        .attr("text-anchor", "center")
+        .text(
+          start_year + " - " + end_year + ": base temperature " + baseTemp + "℃"
+        );
 
-            const yearSpecifier = "Y";
-            const xAxis = d3
-                .axisBottom(xScale)
-                .tickFormat(d => d3.format(yearSpecifier)(d));
-            svg
-                .append("g")
-                .attr("id","x-axis")
-                .style("font-size","14px")
-                .attr("transform",
-                "translate(0,"+(height-padding)+")")
-                .call(xAxis);
+      const xScale = d3
+        .scaleLinear()
+        .domain([start_year, end_year])
+        .range([padding, width - padding]);
 
-            const yScale = d3
-                .scaleTime()
-                .domain([(new Date(1970,0,1)),
-                    (new Date(1970,11,31))])
-                .range([padding,height-padding]);
+      const yearSpecifier = "Y";
+      const xAxis = d3
+        .axisBottom(xScale)
+        .tickFormat((d) => d3.format(yearSpecifier)(d));
+      svg
+        .append("g")
+        .attr("id", "x-axis")
+        .style("font-size", "14px")
+        .attr("transform", "translate(0," + (height - padding) + ")")
+        .call(xAxis);
 
-            
-            const monthSpecifier="%B";
+      const yScale = d3
+        .scaleTime()
+        .domain([new Date(1970, 0, 1), new Date(1970, 11, 31)])
+        .range([padding, height - padding]);
 
-            const yAxis = d3
-                .axisLeft(yScale)
-                .tickFormat(d3.timeFormat(monthSpecifier));
+      const monthSpecifier = "%B";
 
-            svg
-                .append("g")
-                .attr("id","y-axis")
-                .style("font-size","14px")
-                .attr("transform",
-                "translate("+padding+",0)")
-                .call(yAxis);
+      const yAxis = d3
+        .axisLeft(yScale)
+        .tickFormat(d3.timeFormat(monthSpecifier));
 
-                var bar_height=(height-2*padding)/(12);
-                var bar_width=(width-2*padding)/(end_year-start_year);
-            svg
-                .selectAll("rect")
-                .data(data.monthlyVariance)
-                .enter()
-                .append("rect")
-                .attr("x",d=>xScale(d.year))
-                .attr("y",
-                d=>yScale((new Date(1970,d.month-1))))
-                .attr("height",bar_height)
-                .attr("width",bar_width)
-                .attr("class","cell")
-                .style("fill",d=>getGradient(d.variance+baseTemp))
-                .attr("data-month",d=>d.month-1)
-                .attr("data-year",d=>d.year)
-                .attr("data-temp",d=>d.variance+baseTemp);
+      svg
+        .append("g")
+        .attr("id", "y-axis")
+        .style("font-size", "14px")
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(yAxis);
 
-            
+      var bar_height = (height - 2 * padding) / 12;
+      var bar_width = (width - 2 * padding) / (end_year - start_year);
+      var div = d3.select("body")
+        .append("div")
+        .attr("id", "tooltip");
+
+      svg
+        .selectAll("rect")
+        .data(data.monthlyVariance)
+        .enter()
+        .append("rect")
+        .attr("x", (d) => xScale(d.year))
+        .attr("y", (d) => yScale(new Date(1970, d.month - 1)))
+        .attr("height", bar_height)
+        .attr("width", bar_width)
+        .attr("class", "cell")
+        .style("fill", (d) => getGradient(d.variance + baseTemp))
+        .attr("data-month", (d) => d.month - 1)
+        .attr("data-year", (d) => d.year)
+        .attr("data-temp", (d) => d.variance + baseTemp)
+        .on("mouseover",(event,d)=>{
+            var month =d3.timeFormat(monthSpecifier)(new Date(1970,d.month-1));
+            div.transition()
+                .duration(200)
+                .style("opacity",0.9)
+                .attr("data-year",d.year);
+
+            div.html(d.year+" - "+
+            month
+            +"<br/>"+(d.variance+baseTemp).toFixed(1)+"℃"
+            +"<br/>"+d.variance.toFixed(1)+"℃")
+            .style("left", (event.pageX-60)+"px")
+            .style("top", (event.pageY-110)+"px")
+            ;
+        })
+        .on("mouseout",()=>{
+            div.transition()
+                .duration(500)
+                .style("opacity",0);
         });
+    });
 });
 
-function getGradient(d){
-    if(d<=2.8){
-        return "#3D429B";
-    }else if (d<=3.9) {
-        return "#4575B4";
-    }else if (d<=5) {
-        return "#74ADD1";
-    }else if(d<=6.1){
-        return "#ABD9E9";
-    }else if(d<=7.2){
-        return "#E0F3F8";
-    }else if(d<=8.3){
-        return "#FFFFBF";
-    }else if(d<=9.5){
-        return "#FEE090";
-    }else if(d<=10.6){
-        return "#FDAE61";
-    }else if(d<=11.7){
-        return "#F46D43";
-    }else if (d<=12.8) {
-        return "#D73027";
-    }else{
-        return "#A50026";
-    }
+function getGradient(d) {
+  if (d <= 2.8) {
+    return "#3D429B";
+  } else if (d <= 3.9) {
+    return "#4575B4";
+  } else if (d <= 5) {
+    return "#74ADD1";
+  } else if (d <= 6.1) {
+    return "#ABD9E9";
+  } else if (d <= 7.2) {
+    return "#E0F3F8";
+  } else if (d <= 8.3) {
+    return "#FFFFBF";
+  } else if (d <= 9.5) {
+    return "#FEE090";
+  } else if (d <= 10.6) {
+    return "#FDAE61";
+  } else if (d <= 11.7) {
+    return "#F46D43";
+  } else if (d <= 12.8) {
+    return "#D73027";
+  } else {
+    return "#A50026";
+  }
 }
